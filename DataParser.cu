5ce9343ebe6b2c5720ce9cf5f5f8825c737b6759
Pavan Joshi <pavan01121994@gmail.com>
2017-10-25 10:35:10 -0400

    Change in file name: DataParser.cu -> dataparser.cu, change in input array type: double -> float

--- DataParser.cu
+++ DataParser.cu
@@ -1,6 +1,6 @@
 #include "global.h"
 // This file is used to parse the input data and get it to the form that is required.
-void parseCSVData(double values[N][FEATURES]){
+void parseCSVData(float values[N][FEATURES]){
     /*
      * This subroutine parses data from an input file defined by INPUT.
      * It parses a csv file and extracts x, y, z, vx, vy, vz, m in the same order & discards an id field defined in the input file.
@@ -8,7 +8,7 @@ void parseCSVData(double values[N][FEATURES]){
     FILE* stream;
     int idx = 0;
     stream = fopen(INPUT, "r");
-    for(idx = 0; fscanf(stream,"%lf,%lf,%lf,%lf,%lf,%lf,%lf,%*lf\n",
+    for(idx = 0; fscanf(stream,"%f,%f,%f,%f,%f,%f,%f,%*f\n",
                         &values[idx][INDEX_X],
                         &values[idx][INDEX_Y],
                         &values[idx][INDEX_Z],
@@ -18,20 +18,20 @@ void parseCSVData(double values[N][FEATURES]){
                         &values[idx][INDEX_M]) != EOF && idx < N; idx++);
     return;
 }
-void generate3DData(double values[N][FEATURES], int x[2], int y[2], int z[2], int vx[2], int vy[2], int vz[2], int m[2]){
+void generate3DData(float values[N][FEATURES], int x[2], int y[2], int z[2], int vx[2], int vy[2], int vz[2], int m[2]){
     /*
      * This subroutine generates 3-dimensional data given the min and max values for spatial coordinates x, y, z, velocities vx, vy, vz and mass m.
      */
     return;
 }
 int main(){
-    double arr[N][FEATURES];
+    float arr[N][FEATURES];
     int i;
     printf("Hello");
     parseCSVData(arr);
     printf("Hello");
     for(i = 0; i < N; i++){
-        printf("%d %lf\n",i,arr[i][0]);
+        printf("%d %f\n",i,arr[i][0]);
     }
     return 0;
 }
